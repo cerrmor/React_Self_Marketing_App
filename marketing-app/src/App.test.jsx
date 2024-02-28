4fbc4d133e4caea6d5a5923d2332dd699be83e5b
Garrison <garrison.jabs@gmail.com>
2024-02-27 19:36:08 -0700

    Added Vitest testing framework and co dependancys + initial two sample tests

---
+++ marketing-app/src/App.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App title="React" />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
+
+describe("Renders main page correctly", async () => {
+    it("Should render the page correctly", async () => {
+    // Setup
+    render(<App />);
+    const h1 = await screen.queryByText("Vite + React");
+
+    // Expectations
+    expect(h1).toBeInTheDocument();
+    });
+});
